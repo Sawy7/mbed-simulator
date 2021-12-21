b79a30a48b50749a149aa9555cc25e8fd7e6e4ea
Alp Sayin <alpsayin@gmail.com>
2021-12-21 16:24:55 +0300

    Modify serial input demo to use isr

--- demos/stdin/main.cpp
+++ demos/stdin/main.cpp
@@ -1,34 +1,47 @@
 #include "mbed.h"
 
+bool serial_flag = false;
 Serial pc(USBTX, USBRX); // tx, rx
 PwmOut led(p5);
 
 float brightness = 0.5f;
 
-int main() {
+void serial_rx_isr()
+{
+    serial_flag = true;
+}
+
+int main() 
+{
     printf("Keyboard input is mapped to stdin\n");
     printf("Click on the serial output panel, then:\n");
     printf("press 'u' to turn LED1 brightness up, 'd' to turn it down\n");
 
     led = brightness;
+    pc.attach(serial_rx_isr);
 
-    while(1) {
-        char c = pc.getc();
-
-        switch (c) {
-            case 'u':
-                if (brightness <= 0.9f) brightness += 0.1f;
-                led = brightness;
-                pc.printf("Up, brightness is %0.1f\n", brightness);
-                break;
-            case 'd':
-                if (brightness >= 0.1f) brightness -= 0.1f;
-                led = brightness;
-                pc.printf("Down, brightness is %0.1f\n", brightness);
-                break;
-            default:
-                pc.printf("Unrecognized input '%c'\n", c);
-                break;
+    while(1) 
+    {
+        if(serial_flag)
+        {
+            serial_flag = false;
+            char c = pc.getc();
+            switch (c) {
+                case 'u':
+                    if (brightness <= 0.9f) brightness += 0.1f;
+                    led = brightness;
+                    pc.printf("Up, brightness is %0.1f\n", brightness);
+                    break;
+                case 'd':
+                    if (brightness >= 0.1f) brightness -= 0.1f;
+                    led = brightness;
+                    pc.printf("Down, brightness is %0.1f\n", brightness);
+                    break;
+                default:
+                    pc.printf("Unrecognized input '%c'\n", c);
+                    break;
+            }
         }
+        wait_ms(10);
     }
 }
