fd7eacdfde6e8aa58d2e9dee21980d04eae1b50f
Alp Sayin <alpsayin@gmail.com>
2021-12-27 02:00:06 +0300

    Remove unnecessary/deprecated emsdk activate cmd

--- start_mbed_simulator.bash
+++ start_mbed_simulator.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-emsdk activate sdk-fastcomp-1.38.21-64bit
 source "/usr/lib/emsdk/emsdk_env.sh"
 # $(sleep 0.5 && firefox "http://localhost:7829") &
 $(sleep 0.5 && ./start_ssl_proxy.bash) &
