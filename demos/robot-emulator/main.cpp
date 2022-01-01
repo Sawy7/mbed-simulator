4e0873a251b876946155da5b04a96ccb5cab6c6c
Alp Sayin <alpsayin@gmail.com>
2022-01-01 23:24:26 +0300

    Update robot emulator demo code to emulate the robot position&direction with a better algorithm

--- demos/robot-emulator/main.cpp
+++ demos/robot-emulator/main.cpp
@@ -1,4 +1,7 @@
+#define MBED_CONF_MBED_TRACE_ENABLE 1
 #include "mbed.h"
+#include "mbed-trace/mbed_trace.h"
+#define TRACE_GROUP  "cs431"
 #include "stm32f413h_discovery_lcd.h"
 
 typedef void (*func_t)(void);
@@ -8,14 +11,9 @@ PwmOut in1(p21);
 PwmOut in2(p22);
 PwmOut in3(p23);
 PwmOut in4(p24);
-// end motor contron pins
+// end motor control pins
 
 // simulation variables, do not modify
-int tail_x=BSP_LCD_GetXSize()/2;
-int tail_y=BSP_LCD_GetYSize()/2;
-int head_x=BSP_LCD_GetXSize()/2;
-int head_y=BSP_LCD_GetYSize()/2;
-float dir = 0;
 Ticker lcd_ticker;
 // end simulation variables
 
@@ -51,43 +49,83 @@ class Ultrasonic
 };
 
 // lcd drawing task that emulates the robot, do not modify
+#define MOTOR_ACTIVATION_POWER 0.01
+#define ROBOT_HEAD_TO_TAIL_PX  8
+#define ROBOT_SPEED_MULTIPLIER 2
+#define ROBOT_TURN_MULTIPLIER  0.1
+#define MAP MOTOR_ACTIVATION_POWER
 void lcd_draw_task()
 {
+    
+    // simulation variables, do not modify
+    static float tail_x=BSP_LCD_GetXSize()/2;
+    static float tail_y=BSP_LCD_GetYSize()/2;
+    static float head_x=BSP_LCD_GetXSize()/2;
+    static float head_y=BSP_LCD_GetYSize()/2;
+    static float dir = 0;
+    static float mleft = 0.0f;
+    static float mright = 0.0f;
+    
+    // clear LCD
     BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
-    if(in1 >= 0.1 && in3 >= 0.1)
+    BSP_LCD_FillCircle(round(tail_x), round(tail_y), 3);
+    BSP_LCD_FillCircle(round(head_x), round(head_y), 3);
+    
+    // error checking
+    if(in1 >= MAP && in2 >= MAP)
     {
-        BSP_LCD_FillCircle(tail_x, tail_y, 3);
-        tail_x = head_x;
-        tail_y = head_y;
-        head_x += round(sin(dir)*in1*4);
-        head_y += round(cos(dir)*in3*4);
+        tr_err("Invalid power feed to motors: +in1 & +in2");
+        return;
     }
-    else if(in2 >= 0.1 && in4 >= 0.1)
+    if(in3 >= MAP && in4 >= MAP)
     {
-        BSP_LCD_FillCircle(head_x, head_y, 3);
-        head_x = tail_x;
-        head_y = tail_y;
-        tail_x -= round(sin(dir)*in2*4);
-        tail_y -= round(cos(dir)*in4*4);
+        tr_err("Invalid power feed to motors: +in3 & +in4");
+        return;
     }
-    else if(in1 >= 0.1 && in4 >= 0.1)
+    
+    // combining inputs into direction
+    if(in1 >= MAP)
     {
-        dir += 0.1;
-        BSP_LCD_FillCircle(head_x, head_y, 3);
-        head_x = tail_x + round(sin(dir)*in1*10);
-        head_y = tail_y + round(cos(dir)*in4*10);
+        mleft = in1;
     }
-    else if(in2 >= 0.1 && in3 >= 0.1)
+    else if(in2 >= MAP)
     {
-        dir -= 0.1;
-        BSP_LCD_FillCircle(head_x, head_y, 3);
-        head_x = tail_x + round(sin(dir)*in2*10);
-        head_y = tail_y + round(cos(dir)*in3*10);
+        mleft = -in2;
     }
+    else
+    {
+        mleft = 0.0;
+    }
+
+    if(in3 >= MAP)
+    {
+        mright = in3;
+    }
+    else if(in4 >= MAP)
+    {
+        mright = -in4;
+    }
+    else
+    {
+        mright = 0.0;
+    }
+    
+    const float finalpow = mleft + mright;
+    const float powdiff = mleft - mright;
+    // step
+    tail_x += sin(dir)*finalpow*2;
+    tail_y += cos(dir)*finalpow*2;       
+    head_x = tail_x + sin(dir)*ROBOT_HEAD_TO_TAIL_PX;
+    head_y = tail_y + cos(dir)*ROBOT_HEAD_TO_TAIL_PX;
+    // turn
+    dir += powdiff*ROBOT_TURN_MULTIPLIER;
+    tr_debug("mright, mleft = [%.2f, %.2f]; finalpow, powdiff = [%.2f, %.2f]; dir = %.2f", mright, mleft, finalpow, powdiff, dir*180/3.14);
+
+    // drawing
     BSP_LCD_SetTextColor(LCD_COLOR_RED);
-    BSP_LCD_FillCircle(tail_x, tail_y, 3);
+    BSP_LCD_FillCircle(round(tail_x), round(tail_y), 3);
     BSP_LCD_SetTextColor(LCD_COLOR_GREEN);
-    BSP_LCD_FillCircle(head_x, head_y, 3);
+    BSP_LCD_FillCircle(round(head_x), round(head_y), 3);
 }
 
 
@@ -106,29 +144,48 @@ void splitted_wait_ms(int delay_ms)
     }
 }
 
-int main() 
+void robot_emulator_init()
 {
+    mbed_trace_init();       // initialize the trace library
     BSP_LCD_Init();
     /* Clear the LCD */
     BSP_LCD_Clear(LCD_COLOR_WHITE);
-    lcd_ticker.attach(lcd_draw_task, 0.25);
-    
+    lcd_ticker.attach(lcd_draw_task, 0.2);
+    printf("Speed of sound is %d m/s due to JS engine limitations\n", Ultrasonic::SPEED_OF_SOUND);    
+}
+
+// USER GLOBAL CODE SPACE BEGIN
+// your codes should go in here
+
+// USER GLOBAL CODE SPACE END
+
+int main() 
+{
+    // DO NOT REMOVE THIS CALL FROM MAIN!
+    robot_emulator_init();
+
+    // USER MAIN CODE SPACE BEGIN
+    // your codes should go in here
+    // below is an example, you should modify it
+
     while (1) 
     {
-        in1 = 0.8;
+        in1 = 1;
         in2 = 0;
-        in3 = 0.8;
+        in3 = 1;
         in4 = 0;
         splitted_wait_ms(5000);
         in1 = 0;
-        in2 = 0.8;
+        in2 = 0.2;
         in3 = 0;
-        in4 = 0.8;
+        in4 = 0.2;
         splitted_wait_ms(5000);
-        in1 = 0.8;
+        in1 = 0.5;
         in2 = 0;
         in3 = 0;
-        in4 = 0.8;
+        in4 = 0.2;
         splitted_wait_ms(5000);
     }
+
+    // USER MAIN CODE SPACE END
 }
